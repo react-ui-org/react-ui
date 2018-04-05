e3e1d1d3c8cb2cf50e6da140730acd1a522ecb3c
Bedřich Schindler <bedrich@visionapps.cz>
2018-04-05 12:23:12 +0200

    Add index.js file with exported components

---
+++ index.js
@@ -0,0 +1,8 @@
+export { default as Button } from './src/components/Button';
+export { default as Documentation } from './src/components/Documentation';
+export { default as Icon } from './src/components/Icon';
+export { default as Radio } from './src/components/Radio';
+export { default as SelectField } from './src/components/SelectField';
+export { default as TextArea } from './src/components/TextArea';
+export { default as TextField } from './src/components/TextField';
+export { default as UserInputBase } from './src/components/UserInputBase';
