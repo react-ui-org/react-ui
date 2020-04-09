b0b36f1bab7ee43a9f933b9bed559f10a2d39d24
Adam Kudrna <adam@adamkudrna.cz>
2020-04-09 07:27:07 +0200

    Add Toolbar vertical alignment option (#35)

--- src/lib/components/layout/Toolbar/Toolbar.jsx
+++ src/lib/components/layout/Toolbar/Toolbar.jsx
@@ -4,17 +4,44 @@ import styles from './Toolbar.scss';
 
 const Toolbar = (props) => {
   const {
+    align,
     children,
   } = props;
 
+  const alignClass = (value) => {
+    if (value === 'top') {
+      return styles.isToolbarAlignedToTop;
+    }
+
+    if (value === 'middle') {
+      return styles.isToolbarAlignedToMiddle;
+    }
+
+    if (value === 'bottom') {
+      return styles.isToolbarAlignedToBottom;
+    }
+
+    return styles.isToolbarAlignedToBaseline;
+  };
+
   return (
-    <div className={styles.toolbar}>
+    <div
+      className={[
+        styles.toolbar,
+        alignClass(align),
+      ].join(' ')}
+    >
       {children}
     </div>
   );
 };
 
+Toolbar.defaultProps = {
+  align: 'bottom',
+};
+
 Toolbar.propTypes = {
+  align: PropTypes.oneOf(['top', 'middle', 'bottom', 'baseline']),
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
