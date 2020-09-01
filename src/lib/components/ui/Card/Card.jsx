ff873b0ca55796f02b5f72a0deb1dec28f10583b
Adam Kudrna <adam@adamkudrna.cz>
2020-09-01 19:22:21 +0200

    BC: Split `Card` types and variants in separate props and add more variants (#138)

--- src/lib/components/ui/Card/Card.jsx
+++ src/lib/components/ui/Card/Card.jsx
@@ -11,15 +11,31 @@ export const Card = (props) => {
     inList,
     raised,
     type,
+    variant,
   } = props;
 
-  let typeClass = styles.isTypeFlat;
-  if (type === 'warning') {
+  const overriddenVariant = type === null ? variant : 'bordered';
+
+  let typeClass = '';
+  if (type === 'success') {
+    typeClass = styles.isTypeSuccess;
+  } else if (type === 'warning') {
     typeClass = styles.isTypeWarning;
   } else if (type === 'error') {
     typeClass = styles.isTypeError;
-  } else if (type === 'bordered') {
-    typeClass = styles.isTypeBordered;
+  } else if (type === 'help') {
+    typeClass = styles.isTypeHelp;
+  } else if (type === 'info') {
+    typeClass = styles.isTypeInfo;
+  } else if (type === 'note') {
+    typeClass = styles.isTypeNote;
+  }
+
+  let variantClass = '';
+  if (overriddenVariant === 'flat') {
+    variantClass = styles.isVariantFlat;
+  } else if (overriddenVariant === 'bordered') {
+    variantClass = styles.isVariantBordered;
   }
 
   let denseClass = '';
@@ -47,6 +63,7 @@ export const Card = (props) => {
       className={(`
         ${styles.root}
         ${typeClass}
+        ${variantClass}
         ${denseClass}
         ${disabledClass}
         ${inListClass}
@@ -65,7 +82,8 @@ Card.defaultProps = {
   id: undefined,
   inList: false,
   raised: false,
-  type: 'flat',
+  type: null,
+  variant: 'flat',
 };
 
 Card.propTypes = {
@@ -75,7 +93,8 @@ Card.propTypes = {
   id: PropTypes.string,
   inList: PropTypes.bool,
   raised: PropTypes.bool,
-  type: PropTypes.oneOf(['flat', 'bordered', 'warning', 'error']),
+  type: PropTypes.oneOf(['success', 'warning', 'error', 'help', 'info', 'note']),
+  variant: PropTypes.oneOf(['flat', 'bordered']),
 };
 
 export default Card;
