9b324acbffc45abc1966ea9fd2b0c2845c9f6b1c
Adam Kudrna <adam@adamkudrna.cz>
2020-09-03 11:38:49 +0200

    BC: Make `LayoutCenter` more generic: call it simply `Center` and stop it from taking up 100 % of viewport height (#143)
    It's now necessary to set the desired height on the parent of the `Center` layout,
    eg. when you're using `Login` screen component which is expected to spread over
    entire viewport.

--- src/lib/components/layout/Center/__tests__/Center.test.jsx
+++ src/lib/components/layout/Center/__tests__/Center.test.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
-import LayoutCenter from '..';
+import { Center } from '../Center';
 
 describe('rendering', () => {
   it('renders correctly', () => {
-    const tree = shallow(<LayoutCenter />);
+    const tree = shallow(<Center />);
 
     expect(shallowToJson(tree)).toMatchSnapshot();
   });
 
   it('renders correctly with children', () => {
-    const tree = shallow(<LayoutCenter><div>test</div></LayoutCenter>);
+    const tree = shallow(<Center><div>test</div></Center>);
 
     expect(shallowToJson(tree)).toMatchSnapshot();
   });
