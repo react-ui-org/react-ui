aaf05f5a299c98c83368403862bb2da724cbfc68
Adam Kudrna <adam@adamkudrna.cz>
2021-10-27 20:05:42 +0200

    Extend `Button`'s `labelVisibility` with breakpoints (#273)

--- src/lib/components/ui/Button/helpers/getRootLabelVisibilityClassName.js
+++ src/lib/components/ui/Button/helpers/getRootLabelVisibilityClassName.js
@@ -1,6 +1,28 @@
 export default (labelVisibility, styles) => {
-  if (labelVisibility === 'desktop') {
-    return styles.withLabelHiddenMobile;
+  // Intentionally omitting `xs` which means label is visible on all screen sizes.
+
+  if (labelVisibility === 'sm') {
+    return styles.withLabelVisibleSm;
+  }
+
+  if (labelVisibility === 'md') {
+    return styles.withLabelVisibleMd;
+  }
+
+  if (labelVisibility === 'lg') {
+    return styles.withLabelVisibleLg;
+  }
+
+  if (labelVisibility === 'xl') {
+    return styles.withLabelVisibleXl;
+  }
+
+  if (labelVisibility === 'xxl') {
+    return styles.withLabelVisibleXxl;
+  }
+
+  if (labelVisibility === 'xxxl') {
+    return styles.withLabelVisibleXxxl;
   }
 
   if (labelVisibility === 'none') {
