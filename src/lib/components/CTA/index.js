d823c629c6945a6d9b5a01405259f04bc52dac0e
Adam Kudrna <adam@adamkudrna.cz>
2022-01-26 16:17:08 +0100

    Drop rarely used `CTA` and `Center` components in favour of `Grid` and helper classes (#361)

--- src/lib/components/CTA/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CTA } from './CTA';
-export { default as CTACenter } from './CTACenter';
-export { default as CTAEnd } from './CTAEnd';
-export { default as CTAStart } from './CTAStart';
