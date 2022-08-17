58eb04e754e4e87e1cc53b450d05c52db14dab25
Adam Kudrna <adam@adamkudrna.cz>
2022-08-17 19:46:15 +0200

    Limit `Grid` gap props to accept only available spacing values (#402)
    ## Migration Path
    
    For `columnGap` and `rowGap`, reduce all values (responsive or not) in the format `var(--rui-spacing-X)` to corresponding numbers (just `X`), e.g.:
    
    ```jsx
    // Before
    <Grid columnGap="var(--rui-spacing-3)">
    
    // After
    <Grid columnGap={3}>
    ```
    
    (Please mind the correct value type.)

--- src/lib/components/Grid/_helpers/generateResponsiveCustomProperties.js
+++ src/lib/components/Grid/_helpers/generateResponsiveCustomProperties.js
@@ -1,14 +1,22 @@
-export const generateResponsiveCustomProperties = (prop, infix) => {
+const prepareValueByType = (value, type) => {
+  if (type === 'spacing') {
+    return `var(--rui-spacing-${value})`;
+  }
+
+  return value;
+};
+
+export const generateResponsiveCustomProperties = (prop, infix, type = null) => {
   if (typeof prop === 'undefined') {
     return null;
   }
 
   if (typeof prop !== 'object') {
-    return { [`--rui-local-${infix}-xs`]: prop };
+    return { [`--rui-local-${infix}-xs`]: prepareValueByType(prop, type) };
   }
 
   return Object.keys(prop).reduce((acc, breakpoint) => ({
     ...acc,
-    [`--rui-local-${infix}-${breakpoint}`]: prop[breakpoint],
+    [`--rui-local-${infix}-${breakpoint}`]: prepareValueByType(prop[breakpoint], type),
   }), {});
 };
