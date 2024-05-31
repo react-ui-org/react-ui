41ff8c2736c3a95c90397f3835e8723c9e83cfdd
Bedřich Schindler <bedrich.schindler@gmail.com>
2024-05-31 13:01:07 +0200

    Replace `jsdom` with `happy-dom` testing environment to enable `Modal` tests (#461)
    `happy-dom` is used due to missing HtmlDialogElement support in `jest`.
    Just to mention, `happy-dom` provides partial support for dialog element,
    so not all test can be implemented.

--- tests/propTests/tagPropTest.js
+++ tests/propTests/tagPropTest.js
@@ -1,6 +1,6 @@
 export const tagPropTest = [
   [
     { tag: 'section' },
-    (rootElement) => expect(rootElement).toContainHTML('<section'),
+    (rootElement) => expect(rootElement.tagName).toEqual('SECTION'),
   ],
 ];
