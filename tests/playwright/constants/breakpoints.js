5c63ccbfad349da34bb0ff48b70114d2ab16ffb0
Bedřich Schindler <bedrich.schindler@gmail.com>
2025-02-17 09:47:25 +0100

    fixup! Introduce Playwright Component Testing (#857)

--- tests/playwright/constants/breakpoints.js
+++ tests/playwright/constants/breakpoints.js
@@ -1,5 +1,12 @@
 /* eslint-disable sort-keys */
+
+// Keep this file in sync with value in `src/styles/settings/_breakpoints.scss`.
+// Recalculation between px and em is done using the following formula:
+// em = px / font-size
+
 export const breakpoints = {
+  // This value is skipped as its value is zero and might cause issues with the comparison.
+  // Tests for xs breakpoint should be covered separately unless its value is higher than 0.
   // xs: 0,
   sm: 576,
   md: 768,
@@ -8,5 +15,3 @@ export const breakpoints = {
   x2l: 1600,
   x3l: 1920,
 };
-/* eslint-enable sort-keys */
-
