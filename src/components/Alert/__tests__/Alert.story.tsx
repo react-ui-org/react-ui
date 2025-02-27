73c5fab253fa57f2db725904aa43f3b10c9d0f9f
Bedřich Schindler <bedrich.schindler@gmail.com>
2025-02-27 10:29:28 +0100

    Enable TypesScript for Playwright (#587)
    Transform Playwright related files to Typescript and introduce temporary
    configuration files `.eslintrc-ts`, `jest.config-ts.js` and `tsconfig.json`
    that must be removed when whole package is transformed into Playwright.
    
    Due to missing TypeScript support in the rest of the package, `*.spec.tsx`
    and `*.story.tsx` file do not have complete types and might not be compatible
    with strict type checking that should be introduced in #394.
    
    The reason for such change is parallel work on transformation to TypeScript
    and on Playwright tests.

--- src/components/Alert/__tests__/Alert.story.tsx
+++ src/components/Alert/__tests__/Alert.story.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import type { HTMLAttributes } from 'react';
 import { Alert } from '..';
+
+// Types for story component will be improved when we have full TypeScript support
+type AlertForTestProps = HTMLAttributes<HTMLDivElement>;
 
 export const AlertForTest = ({
   ...props
-}) => (
+} : AlertForTestProps) => (
   <Alert
     {...props}
   >
