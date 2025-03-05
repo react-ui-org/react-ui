a649a3d05a0e02bc36f7b9855c35056a4419b5db
Lukas Briza <lukasbriza@seznam.cz>
2025-03-05 09:26:34 +0100

    Transform Action jest tests into playwright (#592)

---
+++ src/components/TextLink/__tests__/TextLink.spec.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+  expect,
+  test,
+} from '@playwright/experimental-ct-react';
+import { propTests } from '../../../../tests/playwright';
+import { TextLinkForTest } from './TextLink.story';
+
+test.describe('TextLink', () => {
+  const testHref = '/test/custom/uri';
+
+  test.describe('visual', () => {
+    [
+      ...propTests.labelPropTest,
+    ].forEach(({
+      name,
+      onBeforeTest,
+      props,
+    }) => {
+      test(name, async ({
+        mount,
+        page,
+      }) => {
+        if (onBeforeTest) {
+          await onBeforeTest(page);
+        }
+
+        const component = await mount(
+          <TextLinkForTest
+            {...props}
+          />,
+        );
+
+        const screenshot = await component.screenshot();
+        expect(screenshot).toMatchSnapshot();
+      });
+    });
+  });
+
+  test.describe('non-visual', () => {
+    test('href', async ({ mount }) => {
+      const component = await mount(
+        <TextLinkForTest href={testHref} />,
+      );
+
+      await expect(component).toHaveAttribute('href', testHref);
+    });
+  });
+
+  test.describe('functionality', () => {
+    test('calls native redirect when clicked on', async ({ mount }) => {
+      const component = await mount(<TextLinkForTest href={testHref} />);
+      const page = component.page();
+
+      await Promise.all([
+        page.waitForURL(testHref),
+        component.click(),
+      ]);
+
+      expect(page.url()).toContain(testHref);
+    });
+
+    test('calls native redirect when Enter pressed', async ({ mount }) => {
+      const component = await mount(<TextLinkForTest href={testHref} />);
+      const page = component.page();
+
+      await Promise.all([
+        page.waitForURL(testHref),
+        component.press('Enter'),
+      ]);
+
+      expect(page.url()).toContain(testHref);
+    });
+
+    test('calls onClick when clicked', async ({ mount }) => {
+      let clicked = false;
+      const component = await mount(
+        <TextLinkForTest
+          onClick={() => {
+            clicked = true;
+          }}
+        />,
+      );
+      await component.click();
+
+      expect(clicked).toBeTruthy();
+    });
+
+    test('calls onClick when Enter pressed', async ({ mount }) => {
+      let clicked = false;
+      const component = await mount(
+        <TextLinkForTest
+          onClick={() => {
+            clicked = true;
+          }}
+        />,
+      );
+      await component.press('Enter');
+
+      expect(clicked).toBeTruthy();
+    });
+  });
+});
