e54c6c505608e79f4c69327eed97b4cd3b311ac7
Lukas Briza <lukasbriza@seznam.cz>
2025-06-11 08:58:16 +0200

    fixup! Transform popup from jest to playwright (#597)

--- src/components/Popover/__tests__/PopoverWrapper.spec.tsx
+++ src/components/Popover/__tests__/PopoverWrapper.spec.tsx
@@ -9,9 +9,6 @@ test.describe('PopoverWrapper', () => {
   test.describe('non-visual', () => {
     const tags = [
       'div',
-      'main',
-      'nav',
-      'section',
       'span',
     ];
 
--- src/components/Popover/__tests__/_propTests/placementPropTest.ts
+++ src/components/Popover/__tests__/_propTests/placementPropTest.ts
@@ -3,10 +3,11 @@ import type { PropTests } from '../../../../../tests/playwright/types';
 const positions = ['top', 'right', 'bottom', 'left'];
 const alignment = [undefined, 'start', 'end'];
 
-export const placementPropTest: PropTests = alignment
-  .map((alignValue) => positions.map((positionValue) => ({
+export const placementPropTest: PropTests = positions.flatMap((positionValue) => alignment.map(
+  (alignValue) => ({
     name: alignValue ? `placement:enum=${positionValue}-${alignValue}` : `placement:enum=${positionValue}`,
     props: {
       placement: alignValue ? `${positionValue}-${alignValue}` : positionValue,
     },
-  }))).reduce((prev, next) => [...prev, ...next]);
+  }),
+));
