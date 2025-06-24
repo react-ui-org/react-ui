e8f978a8cd1669864e41665cdd7d5b8b43bac13c
Lukas Briza <lukasbriza@seznam.cz>
2025-06-24 10:03:04 +0200

    Export isChildrenEmpty from RUI.

--- src/components/Card/CardFooter.jsx
+++ src/components/Card/CardFooter.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { transferProps } from '../../helpers/transferProps';
 import { withGlobalProps } from '../../providers/globalProps';
-import { isChildrenEmpty } from '../_helpers/isChildrenEmpty';
+import { isChildrenEmpty } from '../../helpers/isChildrenEmpty/isChildrenEmpty';
 import styles from './Card.module.scss';
 
 export const CardFooter = ({
