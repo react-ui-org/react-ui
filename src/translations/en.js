3c7e5b4c2711bd73606231e07e46a1dabd441e13
David Menc <david.menc@icloud.com>
2025-06-27 10:33:31 +0200

    Add text when the user is dragging files over FileInputField

--- src/translations/en.js
+++ src/translations/en.js
@@ -5,6 +5,7 @@ export default {
   FileInputField: {
     browse: 'Browse',
     drop: 'or drop file here',
+    dropFileHere: 'Drop file here',
     filesSelected: 'files selected',
   },
   ModalCloseButton: {
