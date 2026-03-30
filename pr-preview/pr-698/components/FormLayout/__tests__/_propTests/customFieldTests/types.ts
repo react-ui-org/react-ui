366dc7071b66dee5e7eef2e518b1431cb21cb2e3
adamkudrna <adamkudrna@users.noreply.github.com>
2026-03-30 20:56:04 +0000

    Remove preview for PR 698 🛬

--- pr-preview/pr-698/components/FormLayout/__tests__/_propTests/customFieldTests/types.ts
+++
@@ -1,9 +0,0 @@
-import type { HTMLAttributes } from 'react';
-import type { PropTest } from '../../../../../../tests/playwright/types';
-
-export type CustomFieldTestType = PropTest & {
-  customFieldLayoutProps: HTMLAttributes<HTMLDivElement>;
-  customFieldProps?: HTMLAttributes<HTMLDivElement>;
-};
-
-export type CustomFieldTestsType = CustomFieldTestType[];
--- pr-preview/pr-698/providers/globalProps/index.js
+++
@@ -1,3 +0,0 @@
-export { default as GlobalPropsContext } from './GlobalPropsContext';
-export { default as GlobalPropsProvider } from './GlobalPropsProvider';
-export { default as withGlobalProps } from './withGlobalProps';
