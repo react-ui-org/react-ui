6a3dbc0ef46149341446f6d432f864836baca087
Bedřich Schindler <bedrich.schindler@gmail.com>
2026-04-02 11:15:40 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! !fixup Introduce `devcontainer` service container based development environment (#705)

--- docker/react_ui_devcontainer/files/home/developer/shell-init.sh
+++ docker/react_ui_devcontainer/files/home/developer/shell-init.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # Block SSH agent forwarding if the environment variable `BLOCK_SSH_AUTH_SOCK` is set to "true"
+#
+# VS Code forwards the SSH agent socket to the dev container by default, so this allows users
+# to opt out of that behavior if they want to disable SSH agent forwarding to isolate the dev container.
+# JetBrains IDEs forward the SSH agent socket only if allowed by the user.
 if [ "$BLOCK_SSH_AUTH_SOCK" = "true" ]; then
   # Disable SSH agent forwarding for the current shell session
   export SSH_AUTH_SOCK=/dev/null
