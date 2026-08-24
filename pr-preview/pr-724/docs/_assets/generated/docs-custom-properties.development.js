8587a21588c8f3c88f588caba89e704ba787c7d9
bedrich-schindler <bedrich-schindler@users.noreply.github.com>
2026-08-24 08:02:50 +0000

    Deploy preview for PR 724 🛫

--- pr-preview/pr-724/docs/_assets/generated/docs-custom-properties.development.js
+++ pr-preview/pr-724/docs/_assets/generated/docs-custom-properties.development.js
@@ -20,46 +20,52 @@ return /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/docsCustomProperties.js":
+/***/ "./src/docsCustomProperties.js"
 /*!*************************************!*\
   !*** ./src/docsCustomProperties.js ***!
   \*************************************/
-/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
 
-eval("{\n\n__webpack_require__(/*! ./theme.scss */ \"./src/theme.scss\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvZG9jc0N1c3RvbVByb3BlcnRpZXMuanMiLCJtYXBwaW5ncyI6Ijs7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL0ByZWFjdC11aS1vcmcvcmVhY3QtdWkvLi9zcmMvZG9jc0N1c3RvbVByb3BlcnRpZXMuanM/MjM3MSJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBUaGlzIGlzIG5lZWRlZCB0byBnZW5lcmF0ZSBDU1MgZmlsZSBjb250YWluaW5nIG9ubHkgQ1NTIGN1c3RvbSBwcm9wZXJ0eSBkZWZpbml0aW9uc1xuLy8gVGhpcyBmaWxlIGlzIG5lZWRlZCB0byBtYWtlIGN1c3RvbSBwcm9wZXJ0eSB2YWx1ZXMgYWNjZXNzaWJsZSBpbiB0aGUgZG9jdW1lbnRhdGlvblxuaW1wb3J0ICcuL3RoZW1lLnNjc3MnO1xuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/docsCustomProperties.js\n\n}");
+eval("{\n\n__webpack_require__(/*! ./theme.scss */ \"./src/theme.scss\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvZG9jc0N1c3RvbVByb3BlcnRpZXMuanMiLCJtYXBwaW5ncyI6Ijs7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL0ByZWFjdC11aS1vcmcvcmVhY3QtdWkvLi9zcmMvZG9jc0N1c3RvbVByb3BlcnRpZXMuanM/MjM3MSJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBUaGlzIGlzIG5lZWRlZCB0byBnZW5lcmF0ZSBDU1MgZmlsZSBjb250YWluaW5nIG9ubHkgQ1NTIGN1c3RvbSBwcm9wZXJ0eSBkZWZpbml0aW9uc1xuLy8gVGhpcyBmaWxlIGlzIG5lZWRlZCB0byBtYWtlIGN1c3RvbSBwcm9wZXJ0eSB2YWx1ZXMgYWNjZXNzaWJsZSBpbiB0aGUgZG9jdW1lbnRhdGlvblxuaW1wb3J0ICcuL3RoZW1lLnNjc3MnO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/docsCustomProperties.js\n\n}");
 
-/***/ }),
+/***/ },
 
-/***/ "./src/theme.scss":
+/***/ "./src/theme.scss"
 /*!************************!*\
   !*** ./src/theme.scss ***!
   \************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-eval("{__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvdGhlbWUuc2NzcyIsIm1hcHBpbmdzIjoiO0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9AcmVhY3QtdWktb3JnL3JlYWN0LXVpLy4vc3JjL3RoZW1lLnNjc3M/NmZmYiJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/theme.scss\n\n}");
+eval("{__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvdGhlbWUuc2NzcyIsIm1hcHBpbmdzIjoiO0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9AcmVhY3QtdWktb3JnL3JlYWN0LXVpLy4vc3JjL3RoZW1lLnNjc3M/NmZmYiJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/theme.scss\n\n}");
 
-/***/ })
+/***/ }
 
 /******/ 	});
 /************************************************************************/
 /******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
+/******/ 	const __webpack_module_cache__ = {};
 /******/ 	
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		const cachedModule = __webpack_module_cache__[moduleId];
 /******/ 		if (cachedModule !== undefined) {
 /******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 		const module = __webpack_module_cache__[moduleId] = {
 /******/ 			// no module.id needed
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
 /******/ 		};
 /******/ 	
 /******/ 		// Execute the module function
+/******/ 		if (!(moduleId in __webpack_modules__)) {
+/******/ 			delete __webpack_module_cache__[moduleId];
+/******/ 			const e = new Error("Cannot find module '" + moduleId + "'");
+/******/ 			e.code = 'MODULE_NOT_FOUND';
+/******/ 			throw e;
+/******/ 		}
 /******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
 /******/ 	
 /******/ 		// Return the exports of the module
@@ -71,7 +77,7 @@ eval("{__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-ext
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
 /******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			if(Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
@@ -83,7 +89,7 @@ eval("{__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-ext
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/docsCustomProperties.js");
+/******/ 	let __webpack_exports__ = __webpack_require__("./src/docsCustomProperties.js");
 /******/ 	
 /******/ 	return __webpack_exports__;
 /******/ })()
