1534cb25eb00ee6db36dd0f18822d8d252202c56
bedrich-schindler <bedrich-schindler@users.noreply.github.com>
2026-08-24 08:05:46 +0000

    Remove preview for PR 724 🛬

--- pr-preview/pr-724/docsCustomProperties.js
+++
@@ -1,3 +0,0 @@
-// This is needed to generate CSS file containing only CSS custom property definitions
-// This file is needed to make custom property values accessible in the documentation
-import './theme.scss';
