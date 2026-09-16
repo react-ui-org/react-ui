c60a8fd46f51391387254face2573dd0c6f5793b
bedrich-schindler <bedrich-schindler@users.noreply.github.com>
2026-09-16 11:42:43 +0000

    Remove preview for PR 743 🛬

--- pr-preview/pr-743/components/Popover/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Popover } from './Popover';
-export { default as PopoverWrapper } from './PopoverWrapper';
-export type {
-  PopoverPlacement,
-  PopoverPlacementStyle,
-  PopoverProps,
-  PopoverWrapperProps,
-} from './Popover.types';
--- pr-preview/pr-743/providers/translations/TranslationsContext.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import defaultTranslations from '../../translations/en';
-import type { Translations } from './Translations.types';
-
-const RUIContext = React.createContext<Translations>(defaultTranslations);
-
-export default RUIContext;
