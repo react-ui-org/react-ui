ff92234285e5ed6b8590330e693aa649eb825b2f
adamkudrna <adamkudrna@users.noreply.github.com>
2026-09-23 15:13:26 +0000

    Remove preview for PR 736 🛬

--- pr-preview/pr-736/types/colors.ts
+++
@@ -1,7 +0,0 @@
-export type ActionColor = 'primary' | 'secondary' | 'selected';
-
-export type FeedbackColor = 'success' | 'warning' | 'danger' | 'help' | 'info' | 'note';
-
-export type NeutralColor = 'light' | 'dark';
-
-export type Color = ActionColor | FeedbackColor | NeutralColor;
