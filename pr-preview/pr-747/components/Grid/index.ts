beb97a4cfa42a11bf29c79d5fe5c90b5a23a7684
bedrich-schindler <bedrich-schindler@users.noreply.github.com>
2026-09-23 08:29:46 +0000

    Remove preview for PR 747 🛬

--- pr-preview/pr-747/components/Grid/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Grid } from './Grid';
-export { default as GridSpan } from './GridSpan';
-export type {
-  GridProps,
-  GridResponsiveValue,
-  GridSpacingValue,
-  GridSpanProps,
-  GridTag,
-} from './Grid.types';
--- pr-preview/pr-747/components/SelectField/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as SelectField } from './SelectField';
-export type {
-  SelectFieldOption,
-  SelectFieldOptionGroup,
-  SelectFieldProps,
-  SelectFieldVariant,
-} from './SelectField.types';
